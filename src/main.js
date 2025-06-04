3bb034ecd46ba11a43d352c06041d49d733cf1f9
Hector <comhector_@hotmail.com>
2025-06-03 20:36:29 -0600

    Terminando Proyecto

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ let addTask = () => {
     let newValue = input.value;
     list.innerHTML += `<div class="task-container" id="${idCounter}">
                             <label for="">
-                                <input type="checkbox" class="checkbox"> ${newValue}
+                                <input type="checkbox" class="checkbox" > ${newValue}
                             </label>
                             <img src="images/bote-de-basura.png" alt="" class="closeBtn">
                         </div>`;
@@ -26,13 +26,31 @@ let addTask = () => {
 };
 
 list.addEventListener('click',(event)=>{
-    console.log(event.target);
+    console.log(event);
+    console.log(event.target.nodeName == 'INPUT');
+    console.log(event.target.nodeName == 'IMG')
+    console.log(event.target.parentNode.id);
+    if (event.target.nodeName == 'INPUT') {
+        updateStats();
+        console.log(event.target.parentNode.id);
+    }else if (event.target.nodeName == 'IMG') {
+        deleteTask(event.target.parentNode.id)
+        console.log("Elemento Borrado")
+    }
 });
 
 
 
 let updateStats = ()=>{
     let element = list.querySelectorAll('div');
-    let checkbox =list.querySelectorAll('input[type="checkbox"]:checked');
+    let checkbox = list.querySelectorAll('input[type="checkbox"]:checked');
     stats.innerHTML = `<p>Tareas pendientes: ${element.length} Tareas Competadas: ${checkbox.length} </p>`
+}
+
+
+//Eliminar una tarea
+let deleteTask = (id)=>{
+    let taskToDelete = document.getElementById(id);
+    list.removeChild(taskToDelete);
+    updateStats();
 }
